62a2278ff2aa1e043fab24d4c6eff0672524a3c6
Adediran <diranadelakun@gmail.com>
2021-10-27 09:57:33 +0100

    YouTube first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube';
+  
+  des: String="";
+  description: String=""
+  
+  uploads: any[]=[];
+// images
+  url:any;
+  urll:any;
+  format:any;
+  onSelectFile(event: any) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      if(file.type.indexOf('image')> -1){
+        this.format = 'image';
+      } else if(file.type.indexOf('video')> -1){
+        this.format = 'video';
+      }
+      reader.onload = (event) => {
+        this.url = (<FileReader>event.target).result;
+      }
+    }
+  }
+
+  selectFile(event:any){
+    if (event.target.files){
+      var reader = new FileReader()
+      reader.readAsDataURL(event.target.files[0])
+      reader.onload = (event: any)=>{
+        this.url = event.target.result
+      }
+    }
+  }
+  show(){
+    this.urll= this.url;
+    this.description = this.des
+    this.uploads.push(this.urll)
+    console.log(this.uploads)
+  }
+    // this.uploads.push(this.url)  
+  
+
+
+
+  // imga(e:any){
+  //   this.img = e.target.value;
+  //   // this.img = this.img.slice(12,0)
+  //   this.img = this.img.replace(/^.*[\\\/]/, '')
+  //   this.des = `../../assets/videos/ ${this.img}`
+
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    LeftnavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
